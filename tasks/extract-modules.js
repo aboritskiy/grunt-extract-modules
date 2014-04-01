68bdbd563a53096a00cbf39bdee3ea950f254b71
aboritskiy <anton.boritskiy@sitewards.com>
2014-04-01 15:12:41 +0200

    - added support for modules with equal names - added optional printing of module reference table

--- tasks/extract-modules.js
+++ tasks/extract-modules.js
@@ -21,22 +21,52 @@ module.exports = function(grunt) {
             separator: '\n'
         });
 
+        var oFileModuleReference = {};
+
         // Iterate over all specified file groups.
         this.files.forEach(function(fileObject) {
-            fileObject.src.filter(function(filepath) {
+            fileObject.src.filter(function(sFilepath) {
                 // Warn on and remove invalid source files (if nonull was set).
-                if (!grunt.file.exists(filepath)) {
-                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                if (!grunt.file.exists(sFilepath)) {
+                    grunt.log.warn('Source file "' + sFilepath + '" not found.');
                     return false;
                 } else {
                     return true;
                 }
-            }).map(function(filepath) {
-                var oFileParser = new FileParser(filepath);
+            }).map(function(sFilepath) {
+                grunt.log.writeln('Processing page ' + sFilepath);
+
+                var oFileParser = new FileParser(sFilepath);
                 var oFileWriter = new FileWriter(oFileParser);
 
                 oFileWriter.writeModuleData(fileObject.dest);
+
+                oFileModuleReference[sFilepath] = [];
+
+                for(var sModuleName in oFileParser.oGlobalModuleIndex) {
+                    if (oFileParser.oGlobalModuleIndex.hasOwnProperty(sModuleName)) {
+                        oFileModuleReference[sFilepath].push(sModuleName);
+                    }
+                }
             });
         });
+
+        if (this.data.moduleref) {
+            var sReferenceContent = '';
+            for(var sFilepath in oFileModuleReference) {
+                if (oFileModuleReference.hasOwnProperty(sFilepath)) {
+                    sReferenceContent += '\n\nModules of ' + sFilepath;
+                    var oFileModuleIndex = oFileModuleReference[sFilepath];
+
+                    if (this.data.sortmoduleref) {
+                        oFileModuleIndex.sort();
+                    }
+                    for (var i = 0; i < oFileModuleIndex.length; i++) {
+                        sReferenceContent += '\n ' + oFileModuleIndex[i];
+                    }
+                }
+            }
+            grunt.file.write(this.data.moduleref, sReferenceContent);
+        }
     });
 };
